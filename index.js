26d263824679fa31da039ae27ea370319d3bd81d
Arya yadav <151600217+Arya-yadav-018@users.noreply.github.com>
2025-10-17 18:01:33 +0530

    Fix: Updated CORS for Vercel domain

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173", // frontend origin
+  origin: ["http://localhost:5173" , "https://ai-image-generator-2uu8.vercel.app/"], // frontend origin
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
